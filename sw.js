5bf74dcd43a8d7154c553ade14c3a4f86a78787f
Josh007j <152661710+Josh007j@users.noreply.github.com>
2025-12-30 14:00:47 -0800

    Add files via upload
    initial portfolio launch

---
+++ sw.js
@@ -0,0 +1,10 @@
+const cacheName = 'arome-v1';
+const assets = ['/', '/index.html', '/logo.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
